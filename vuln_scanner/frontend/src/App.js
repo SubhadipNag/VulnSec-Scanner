93102701c7c07ee789dc2a1a83de064976483174
Mr.L0s3r09 <78735419+SubhadipNag@users.noreply.github.com>
2026-05-18 13:26:25 +0530

    Add files via upload

---
+++ vuln_scanner/frontend/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import ScanForm from "./components/ScanForm";
+
+function App() {
+
+  return (
+
+    <div className="App">
+
+      <h1>Web Vulnerability Scanner</h1>
+
+      <ScanForm />
+
+    </div>
+
+  );
+}
+
+export default App;
